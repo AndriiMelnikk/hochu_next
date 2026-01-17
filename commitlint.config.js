0b3cbeab8fa458c10f6c1f7ba086cf1b9aa9705a
Andrii Melnyk <andrii_melnyk@MacBook-Air-Andrii.local>
2026-01-17 10:23:32 +0200

    chore: setup husky, commitlint and lint-staged

---
+++ commitlint.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "ci",
+        "build",
+        "revert",
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [2, "never", ["upper-case", "pascal-case", "start-case"]],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 72],
+    "body-leading-blank": [2, "always"],
+    "body-max-line-length": [2, "always", 72],
+    "footer-leading-blank": [2, "always"],
+  },
+};
